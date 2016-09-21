c1af6983f1cc89ff08c7e8ad24dcb78a8f9d581a
Gordon <civfactions@gmail.com>
2016-09-21 11:47:10 -0500

    Add basic project framework Update pom to java 8 and spigot 1.10.2

---
+++ src/main/java/com/devotedmc/ExilePearl/ExilePearlPlugin.java
@@ -0,0 +1,41 @@
+package com.devotedmc.ExilePearl;
+
+import vg.civcraft.mc.civmodcore.ACivMod;
+
+/**
+ * An offshoot of Civcraft's PrisonPearl plugin
+ * @author GordonFreemanQ, aleks
+ *
+ */
+public class ExilePearlPlugin extends ACivMod {
+	
+	
+	public ExilePearlPlugin() {
+		
+	}
+
+	/**
+	 * Spigot enable method
+	 */
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+
+	/**
+	 * Spigot disable method
+	 */
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
+
+	/**
+	 * Gets the plugin name
+	 */
+	@Override
+	protected final String getPluginName() {
+		return "ExilePearl";
+	}
+
+}
