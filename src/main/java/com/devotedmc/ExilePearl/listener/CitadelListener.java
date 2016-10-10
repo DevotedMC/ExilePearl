ab26c58c576af22b21094c7a6c95fb7e25b63b62
Gordon <civfactions@gmail.com>
2016-10-10 07:14:40 -0500

    Add acid block event listener

--- src/main/java/com/devotedmc/ExilePearl/listener/CitadelListener.java
+++ src/main/java/com/devotedmc/ExilePearl/listener/CitadelListener.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventPriority;
 import com.devotedmc.ExilePearl.ExilePearlApi;
 import com.devotedmc.ExilePearl.ExileRule;
 
+import vg.civcraft.mc.citadel.events.AcidBlockEvent;
 import vg.civcraft.mc.citadel.events.ReinforcementDamageEvent;
 import vg.civcraft.mc.citadel.reinforcement.PlayerReinforcement;
 
@@ -18,7 +19,7 @@ public class CitadelListener extends RuleListener {
 
 	/**
 	 * Prevents exiled players from damaging reinforcements
-	 * @param e The event
+	 * @param e The event args
 	 */
 	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
 	public void onReinforcementDamage(ReinforcementDamageEvent e) {
@@ -31,6 +32,16 @@ public class CitadelListener extends RuleListener {
 			return;
 		}		
 		
+		checkAndCancelRule(ExileRule.DAMAGE_REINFORCEMENT, e, e.getPlayer());
+	}
+	
+	/**
+	 * Prevents exiled players from using acid blocks
+	 * Uses the same rule as damaging reinforcements
+	 * @param e The event args
+	 */
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+	public void onAcidBlockEvent(AcidBlockEvent e) {
 		checkAndCancelRule(ExileRule.DAMAGE_REINFORCEMENT, e, e.getPlayer());
 	}
 }
