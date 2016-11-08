8638a671166981c9581f9448dd69439a77524da2
Gordon <civfactions@gmail.com>
2016-11-08 09:51:31 -0600

    fix pom

--- src/main/java/com/devotedmc/ExilePearl/command/CmdAdminExileAny.java
+++ src/main/java/com/devotedmc/ExilePearl/command/CmdAdminExileAny.java
@@ -3,7 +3,6 @@ package com.devotedmc.ExilePearl.command;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
