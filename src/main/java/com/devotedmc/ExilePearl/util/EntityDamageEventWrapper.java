5074a80ac982d452aebebecbaa9c98646a9576ff
Gordon <civfactions@gmail.com>
2016-11-17 09:47:05 -0600

    simplify projectile check

--- src/main/java/com/devotedmc/ExilePearl/util/EntityDamageEventWrapper.java
+++ src/main/java/com/devotedmc/ExilePearl/util/EntityDamageEventWrapper.java
@@ -1,11 +1,7 @@
 package com.devotedmc.ExilePearl.util;
 
-import org.bukkit.entity.Arrow;
-import org.bukkit.entity.Egg;
-import org.bukkit.entity.Fireball;
-import org.bukkit.entity.FishHook;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Snowball;
+import org.bukkit.entity.Projectile;
 import org.bukkit.entity.Wolf;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
@@ -43,30 +39,10 @@ public class EntityDamageEventWrapper {
 			if (wolf.getOwner() instanceof Player) {
 				damager = (Player) wolf.getOwner();
 			}
-		} else if (event.getDamager() instanceof Arrow) {
-			Arrow arrow = (Arrow) event.getDamager();
-			if (arrow.getShooter() instanceof Player) {
-				damager = (Player) arrow.getShooter();
-			}
-		} else if (event.getDamager() instanceof Snowball) {
-			Snowball snowball = (Snowball)event.getDamager();
-			if (snowball.getShooter() instanceof Player) {
-				damager = (Player) snowball.getShooter();
-			}
-		} else if (event.getDamager() instanceof Egg) {
-			Egg egg = (Egg)event.getDamager();
-			if (egg.getShooter() instanceof Player) {
-				damager = (Player) egg.getShooter();
-			}
-		} else if (event.getDamager() instanceof Fireball) {
-			Fireball fireball = (Fireball)event.getDamager();
-			if (fireball.getShooter() instanceof Player) {
-				damager = (Player) fireball.getShooter();
-			}
-		} else if (event.getDamager() instanceof FishHook) {
-			FishHook fishHook = (FishHook)event.getDamager();
-			if (fishHook.getShooter() instanceof Player) {
-				damager = (Player) fishHook.getShooter();
+		} else if (event.getDamager() instanceof Projectile) {
+			Projectile projectile = (Projectile) event.getDamager();
+			if (projectile.getShooter() instanceof Player) {
+				damager = (Player) projectile.getShooter();
 			}
 		}
 		return damager;
