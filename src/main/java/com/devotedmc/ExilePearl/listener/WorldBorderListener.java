843e29be097226c47bb38dde42bd809bf4fe1e86
BuildTools <squareblob@protonmail.com>
2020-08-23 13:27:29 -0400

    Warn players moving pearls outside worldborder

--- src/main/java/com/devotedmc/ExilePearl/listener/WorldBorderListener.java
+++ src/main/java/com/devotedmc/ExilePearl/listener/WorldBorderListener.java
@@ -1,6 +1,7 @@
 package com.devotedmc.ExilePearl.listener;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -9,6 +10,7 @@ import com.devotedmc.ExilePearl.ExilePearlApi;
 import com.devotedmc.ExilePearl.ExilePearlPlugin;
 import com.devotedmc.ExilePearl.PearlFreeReason;
 import com.devotedmc.ExilePearl.event.PearlDecayEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class WorldBorderListener extends RuleListener {
 
@@ -34,6 +36,25 @@ public class WorldBorderListener extends RuleListener {
 			Bukkit.getScheduler().scheduleSyncDelayedTask(ExilePearlPlugin.getApi(), () -> {
 				pearlApi.freePearl(e.getPearl(), PearlFreeReason.OUTSIDE_WORLD_BORDER);
 			});
+		}
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onInventoryClick(InventoryClickEvent e) {
+		boolean autoFree = pearlApi.getPearlConfig().getShouldAutoFreeWorldBorder();
+		if (!autoFree) {
+			return;
+		}
+		boolean clickedTop = e.getRawSlot() < e.getView().getTopInventory().getSize();
+		if (clickedTop) {
+			return;
+		}
+		if (pearlApi.getPearlFromItemStack(e.getCurrentItem()) != null) {
+			Location loc = e.getView().getTopInventory().getLocation();
+			if (loc != null && !pearlApi.isLocationInsideBorder(loc)) {
+				e.getWhoClicked().sendMessage(ChatColor.RED + "Pearls placed outside of the world border will be freed");
+				e.setCancelled(true);
+			}
 		}
 	}
 }
