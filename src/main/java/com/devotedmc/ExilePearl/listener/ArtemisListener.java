2fcc523bb5e98da2129c5b1f4b9f05eddddf258e
Cola <vaultslave@gmail.com>
2021-01-15 07:41:20 +0000

    Add msgs

--- src/main/java/com/devotedmc/ExilePearl/listener/ArtemisListener.java
+++ src/main/java/com/devotedmc/ExilePearl/listener/ArtemisListener.java
@@ -2,6 +2,7 @@ package com.devotedmc.ExilePearl.listener;
 
 import com.devotedmc.ExilePearl.ExilePearlApi;
 import com.github.maxopoly.artemis.events.PlayerAttemptLeaveShard;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.inventory.ItemStack;
 
@@ -19,12 +20,14 @@ public class ArtemisListener extends RuleListener {
 	public void onPlayerShardTransfer(PlayerAttemptLeaveShard event) {
 		if (pearlApi.isPlayerExiled(event.getPlayer())) {
 			event.setCancelled(true);
+			event.getPlayer().sendMessage(ChatColor.RED + "Exiled players cannot travel between shards.");
 			return;
 		}
 
 		for (ItemStack itemStack : event.getPlayer().getInventory()) {
 			if (pearlApi.getPearlFromItemStack(itemStack) != null) {
 				event.setCancelled(true);
+				event.getPlayer().sendMessage(ChatColor.RED + "You cannot carry pearls across shards! Store the pearl before retrying.");
 				return;
 			}
 		}
